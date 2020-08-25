0a8cbca342fe0ba1e77a1d28b2d6fb6a04040214
AemonT <36158197+AemonT@users.noreply.github.com>
2020-08-25 15:51:21 +0200

    Update AppDelegate.m

--- FlutterApp/ios/Runner/AppDelegate.m
+++ FlutterApp/ios/Runner/AppDelegate.m
@@ -5,7 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GMSServices provideAPIKey:@"AIzaSyDbaw3RwVd-QWE0rp9cC06RW-R9gnCsWZM"];
+  [GMSServices provideAPIKey:@""];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
